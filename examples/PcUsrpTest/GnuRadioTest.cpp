0e5c1ad2f9a0478d26453aba6f05f9c8ae072ed5
Ryan Seal <rlseal@gmail.com>
2008-03-13 16:57:12 -0400

    Adding SimpleHeader file system and Time stamping information to project.

--- examples/PcUsrpTest/GnuRadioTest.cpp
+++ examples/PcUsrpTest/GnuRadioTest.cpp
@@ -3,76 +3,99 @@
 #include <gnuradar/GnuRadarDevice.h>
 #include <gnuradar/GnuRadarSettings.h>
 #include <gnuradar/SThread.h>
+#include <simpleHeader/Shs.h>
+#include <simpleHeader/Time.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
 
-class Consume: public SThread{
-    ProducerConsumerModel& pcModel_;
-    void* buffer_;
-public:
-    Consume(ProducerConsumerModel& pcModel, void* buffer): pcModel_(pcModel),
-								 buffer_(buffer){
-	this->Start();
-    }
-
-    virtual void Run(){
-	while(true){
-	    pcModel_.RequestData(buffer_);
-	}
-    }
-};
-//patch-in class for now. Fix later
-// class Console: public SThread{
-//     std::string prompt_;
-//     std::string input_;
-//     bool quit_;
-
-// public:
-//     Console():prompt_(">>>"), input_(""), quit_(false){}
-
-//     virtual void Run(){
-// 	while(!quit_){
-// 	    cout << prompt_ << endl;
-// 	    cin >> input_;
-// 	    if(input_ == "quit") quit_ = true;
-// 	}
-//     }
-
-//     const bool Running() { return !quit_;}
-// };
 
 int main(){
 
-    //Initialize classes
-    const int kb = 1024;
-    const int Mb = kb*kb;
-    const int bufferSize = 100*kb;
-    const int numBuffers = 10;
+    typedef SimpleHeader<short,2> SimpleHeaderSystem;
+    const string dataSet = "UIUC-13mar08";
+
+    const int    Kb            = 1024;
+    const int    Mb            = Kb*Kb;
+    const double MHz           = 1e6;
+    const double sampleRate    = 64*MHz;
+    const double bandWidth     = 2*MHz;
+    const int    decimation    = sampleRate / bandWidth;
+    const double outputRate    = sampleRate / decimation;
+    const int    numChannels   = 2;
+    const int    BPS           = outputRate*4;
+    const int    bufferSize    = BPS;
+    const int    numBuffers    = 10;
 
     //create consumer buffer - destination 
     int* buffer = new int[bufferSize/sizeof(int)];
 
-    //Parse GNURadio configuration file here (gcf) with Parser Class
+    //50MHz RF with 64MHz sampling - positive image at -14MHz (reversed at +14MHz)
+    vector<double> tuningFreq;
+    tuningFreq.push_back(-14e6);
+    tuningFreq.push_back(-14e6);
+
+    cout << "--------------------Settings----------------------" << endl;
+    cout << "Sample Rate                 = " << sampleRate << endl;
+    cout << "Bandwidth                   = " << bandWidth  << endl;
+    cout << "Decimation                  = " << decimation << endl;
+    cout << "Output Rate                 = " << outputRate << endl;
+    cout << "Number of Channels          = " << numChannels << endl;
+    cout << "Buffer size (bytes)         = " << static_cast<double>(BPS) << endl;
+    for(int i=0; i<numChannels; ++i)
+	cout << "Channel[" << i << "] Tuning Frequency = " << tuningFreq[i] << endl;
+    cout << "--------------------Settings----------------------\n\n" << endl;
+
+    //time stamp class
+    Time currentTime;
+
+    //write a test file for demonstration purposes
+    SimpleHeaderSystem* 
+	header = new SimpleHeaderSystem(dataSet, File::WRITE, File::BINARY);
+
+    //build the primary header
+    header->primary.Title("UIUC-GnuRadio-Test");
+    header->primary.Description("March 13 2008 Test data");
+    header->primary.Add("Instrument", "GNURadio Rev 4", "Receiving Instrument");
+    header->primary.Add("Time", currentTime.GetTime(), "Experiment Starting Time (CDT)");
+    header->primary.Add("Sample Rate", sampleRate, "Sample Clock Rate");
+    header->primary.Add("Bandwidth", bandWidth, "System Bandwidth");
+    header->primary.Add("Decimation", decimation, "System Decimation");
+    header->primary.Add("Channels", numChannels, "Number of Channels Used");
+    header->primary.Add("Output Rate", outputRate, "System Output Rate");
+
+    //write primary header to disk
+    header->primary.ProgramInfo(header->file.GetRef());
+    header->WritePrimary();
+    (header->file.GetRef()).flush();
+
+    //Program GNURadio 
     GnuRadarSettings settings;
-    settings.Tune(0,10e6);
-    settings.decimationRate = 16;
+    for(int i=0; i<numChannels; ++i){
+	settings.Tune(i,tuningFreq[i]);
+    }
+    settings.decimationRate = decimation;
     settings.fUsbBlockSize = 0;
     settings.fUsbNblocks = 0;
 
+    //Initialize GNURadarDevice class
     GnuRadarDevice grDevice(settings);
 
-    ProducerConsumerModel pcmodel(bufferSize,buffer,numBuffers,sizeof(int),"testBuffer",grDevice);
+    //Initialize Producer/Consumer Model
+    ProducerConsumerModel pcmodel(
+	bufferSize,
+	buffer,
+	numBuffers,
+	sizeof(int),
+	"GnuRadar",
+	grDevice);
+
+    //Start Producer/Consumer Model 
     pcmodel.Start();
-    int i=0;
-  
-    //Consume consume(pcmodel,buffer);
 
-    // while(true){ sleep(10);}
-    //Console console;
-    //console.Start();
     pcmodel.RequestData(buffer);
-    //while(true){ pcmodel.RequestData(buffer);}
 
-//    pcmodel.Stop();
-    pcmodel.Wait();
     return 0;
 };
 

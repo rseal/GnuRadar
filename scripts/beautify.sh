15610ec692f8073c482e58770e4da637f5931f7d
Ryan Seal <rlseal@gmail.com>
2012-08-23 21:52:29 -0400

    Updated headers.

--- scripts/beautify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-astyle -A4 -rpPn ../include/*.h*
-astyle -A4 -rpPn ../programs/*.cxx*
-astyle -A4 -rpPn ../programs/*.h*
-astyle -A4 -rpPn ../programs/java/com/*.java
--- scripts/gnuradar_configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./bootstrap
-./configure --enable-usrp --enable-gruel --enable-docs --disable-all-components
-make
-make install
